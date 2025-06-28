a449807fdf2f6d384b4b90e01bf84c06d4d27b45
Dilshad <helloshad3@gmail.com>
2025-06-28 13:15:04 +0530

    import change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "./components/ui/sonner";
-import { WeatherDashboard } from "./pages/weather-dashboard";
-import { Layout } from "./components/layout";
-import { ThemeProvider } from "./context/theme-provider";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Toaster } from "./components/ui/sonner";
+import { ThemeProvider } from "./context/theme-provider";
 import { CityPage } from "./pages/city-page";
+import { WeatherDashboard } from "./pages/weather-dashboard";
 
 const queryClient = new QueryClient({
   defaultOptions: {
